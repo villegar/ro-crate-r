154a2e10bde41be536cbc396eb93859ed7bfeea9
Roberto Villegas-Diaz <r.villegas-diaz@liverpool.ac.uk>
2025-08-08 14:19:26 +0100

    Add functions to create minimal RO-Crate objects

---
+++ R/rocrate.R
@@ -0,0 +1,108 @@
+#' Create a new RO-Crate object
+#'
+#' Create a new RO-Crate object. This object includes basic skeleton for the
+#' RO-Crate metadata descriptor (`ro-crate-metadata.json`) file, as described
+#' in the official documentation: https://w3id.org/ro/crate/1.2 >
+#' [Root Data Entity](https://www.researchobject.org/ro-crate/specification/1.2/root-data-entity.html).
+#'
+#' @param ... Optional entities to include in the RO-Crate (e.g., author).
+#' @param context String with URL to the version of the RO-Crate specification
+#'     to use. The context brings the defined terms into the metadata document.
+#' @param conformsTo String with URL to the version of the RO-Crate
+#'     specification which this object conforms to. Conformance declares which
+#'     RO-Crate conventions of using those terms are being followed.
+#'
+#' @returns RO-Crate object, list with an additional class, {rocrate}.
+#' @export
+#'
+#' @examples
+#' rocrateR::rocrate()
+rocrate <- function(...,
+                    context = "https://w3id.org/ro/crate/1.2/context",
+                    conformsTo = gsub("\\/context$", "\\1", context)) {
+  new_ro_crate <- list(
+    `@context` = context,
+    `@graph` = list(
+      list(
+        `@type` = "CreativeWork",
+        `@id` = "ro-crate-metadata.json",
+        about = list(`@id` = "./"),
+        conformsTo = list(`@id` = conformsTo),
+      ),
+      list(
+        `@id` = "./",
+        `@type` = "Dataset"#,
+        # hasPart = list(),
+      )
+    )
+  )
+
+  # capture additional entities
+  extra_entities <- .capture_extra_entities(...)
+  extra_entities_tbl <- tibble::tibble(
+    ent = extra_entities,
+    ent_name = names(extra_entities),
+    required = list(c("@id", "@type"))
+  )
+
+  # validate any additional entities
+  idx <- extra_entities_tbl |>
+    purrr::pmap_lgl(.validate_entity)
+
+  # combine the base crate with any extra entities
+  new_ro_crate$`@graph` <- c(new_ro_crate$`@graph`, unname(extra_entities[idx]))
+
+  # set class for the new object
+  class(new_ro_crate) <- c("rocrate", class(new_ro_crate))
+
+  return(new_ro_crate)
+}
+
+#' Create a new 5 Safes RO-Crate object
+#'
+#' Create a new 5 Safes RO-Crate object. This object includes basic skeleton
+#' for the RO-Crate metadata descriptor (`ro-crate-metadata.json`) file, as
+#' described in the official documentation: https://w3id.org/ro/crate/1.2 >
+#' [Root Data Entity](https://www.researchobject.org/ro-crate/specification/1.2/root-data-entity.html).
+#' Additionally, it includes a profile for the 5 Safes RO-Crate:
+#' https://w3id.org/5s-crate/0.4
+#'
+#' @inheritParams rocrate
+#' @param v5scrate Numeric value with the version of the 5 Safes RO-Crate
+#'     profile to use.
+#'
+#' @returns 5 Safes RO-Crate object, list with an additional class, {rocrate}.
+#' @export
+#'
+#' @examples
+#' rocrateR::rocrate_5s()
+rocrate_5s <- function(...,
+                       context = "https://w3id.org/ro/crate/1.2/context",
+                       conformsTo = gsub("\\/context$", "\\1", context),
+                       v5scrate = 0.4) {
+  # create entity for the 5 safes profile
+  v5scrate_id <- paste0("https://w3id.org/5s-crate/", v5scrate)
+  prof_5scrate <- list(
+    `@id` = v5scrate_id,
+    `@type` = "Profile",
+    name = "Five Safes RO-Crate profile"
+  )
+
+  # create basic RO crate
+  new_ro_crate <- rocrate(..., context = context, conformsTo = conformsTo)
+  # c(list(prof_5scrate = prof_5scrate), extra_entities)
+
+  # attach the 5 safes profile entity
+  new_ro_crate$`@graph` <- c(new_ro_crate$`@graph`, prof_5scrate)
+
+  # update the root entity's conformsTo property
+  idx <- new_ro_crate$`@graph` |>
+    purrr::map_lgl(\(x) x$`@id` == "./" && is.null(getElement(x, "conformsTo")))
+  conformsTo <- list(`@id` = paste0("https://w3id.org/5s-crate/", v5scrate))
+  new_ro_crate$`@graph`[idx][[1]]$conformsTo <- conformsTo
+  new_ro_crate <- new_ro_crate |>
+    add_value(id = "./", key = "conformsTo", conformsTo)
+
+  # return the new RO crate with the 5 safes profile
+  return(new_ro_crate)
+}
