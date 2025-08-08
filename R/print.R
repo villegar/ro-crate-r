bfb68b066b974728a940a94b394f30145ddd137b
Roberto Villegas-Diaz <r.villegas-diaz@liverpool.ac.uk>
2025-08-08 14:38:33 +0100

    Add S3 method to print objects with the 'rocrate' class

---
+++ R/print.R
@@ -0,0 +1,29 @@
+#' Print RO-crate
+#'
+#' Print RO-crate, S3 method for class 'rocrate'. Creates a temporal JSON file,
+#' which then is displayed with the [message] function.
+#'
+#' @param x RO-Crate object, see [rocrateR::rocrate].
+#' @param ... Optional arguments, not used.
+#'
+#' @returns Invisibly the input RO-Crate, `x`.
+#' @export
+#'
+#' @examples
+#' rocrateR::rocrate()
+print.rocrate <- function(x, ...) {
+  # save the input into intermediate JSON file
+  tmp_file <- tempfile(fileext = ".json")
+  # delete temporary file
+  on.exit(unlink(tmp_file, recursive = TRUE, force = TRUE))
+  # store RO-Crate in JSON format
+  jsonlite::write_json(x, path = tmp_file, pretty = TRUE, auto_unbox = TRUE)
+  # load formatte RO-Crate as text
+  rocrate_txt <- readLines(tmp_file)
+  # display formatted RO-Crate
+  rocrate_txt |>
+    paste0(collapse = "\n")|>
+    message()
+  # return (invisibly the input object)
+  invisible(x)
+}
