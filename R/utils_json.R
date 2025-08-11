893369f3802eccfdc46e1abcf4b353f82c6e397c
Roberto Villegas-Diaz <r.villegas-diaz@liverpool.ac.uk>
2025-08-11 16:27:58 +0100

    Add function read_rocrate

--- R/utils_json.R
+++ R/utils_json.R
@@ -1,3 +1,24 @@
+#' Wrapper for [jsonlite::read_json]
+#'
+#' Wrapper for [jsonlite::read_json]. Enforces that the object read is an
+#'     RO-Crate.
+#'
+#' @inheritParams jsonlite::read_json
+#' @inheritDotParams jsonlite::fromJSON
+#'
+#' @returns Invisibly the RO-Crate stored in `path`.
+#' @export
+read_rocrate <- function(path, simplifyVector = FALSE, ...) {
+  # reads the input file as a JSON file
+  rocrate <- jsonlite::read_json(path, simplifyVector = simplifyVector, ...)
+  # assigns it the 'rocrate' class
+  class(rocrate) <- c("rocrate", class(rocrate))
+  # checks the object has the basic structure of an RO-Crate
+  is_rocrate(rocrate)
+  # returns the new object as an RO-crate
+  return(invisible(rocrate))
+}
+
 #' Wrapper for [jsonlite::write_json]
 #'
 #' Wrapper for [jsonlite::write_json]. Enforces that the input object is an
