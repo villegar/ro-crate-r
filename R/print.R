fe6573c2db5f1b1dd476b47bdadf23a3f7c0c6e1
Roberto Villegas-Diaz <r.villegas-diaz@liverpool.ac.uk>
2025-08-11 15:20:33 +0100

    Update print S3 method

--- R/print.R
+++ R/print.R
@@ -12,18 +12,20 @@
 #' @examples
 #' rocrateR::rocrate()
 print.rocrate <- function(x, ...) {
+  # check the `x` object
+  is_rocrate(x)
   # save the input into intermediate JSON file
   tmp_file <- tempfile(fileext = ".json")
   # delete temporary file
   on.exit(unlink(tmp_file, recursive = TRUE, force = TRUE))
   # store RO-Crate in JSON format
   jsonlite::write_json(x, path = tmp_file, pretty = TRUE, auto_unbox = TRUE)
-  # load formatte RO-Crate as text
+  # load formatted RO-Crate as text
   rocrate_txt <- readLines(tmp_file)
   # display formatted RO-Crate
   rocrate_txt |>
     paste0(collapse = "\n")|>
     message()
-  # return (invisibly the input object)
+  # return (invisibly) the input object
   invisible(x)
 }
