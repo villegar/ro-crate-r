140202be6f717198391127a0dd6bb2e92ba8d864
Roberto Villegas-Diaz <r.villegas-diaz@liverpool.ac.uk>
2025-08-11 14:40:18 +0100

    Add generic function and corresponding S3 methods for .validate_entity. Add function to find of entity index based on @id in RO-Crate

--- R/utils-entity.R
+++ R/utils-entity.R
@@ -21,23 +21,84 @@
   return(extra_entities_lst)
 }
 
+#' Find `@id` index in RO-Crate
+#'
+#' Find `@id` index in RO-Crate. Useful to update a component of an entity in
+#' the RO-Crate, add new component (e.g., author + corresponding `@id`).
+#'
+#' @inheritParams add_entity_value
+#'
+#' @returns Boolean vector with index for entity with `@id`.
+#' @keywords internal
+.find_id_index <- function(rocrate, id) {
+  # check the `rocrate` object
+  is_rocrate(rocrate)
+
+  # find the index in `@graph` with the matching {id}
+  getElement(rocrate, "@graph") |>
+    sapply(\(x) getElement(x, "@id") == id)
+  # idx <- rocrate$`@graph` |>
+  #   sapply(\(x) x$`@id` == id)
+  # # verify that only one index was found for the matching {id}
+  # if(sum(idx) != 1) {
+  #   stop("Please, ensure the given {id} is unique and part of the RO-Crate.")
+  # }
+  # return(idx)
+}
+
 #' Validate entity
 #'
-#' @param ent List with new entity.
+#' @param x New entity. If a `character` is given, this is assumed to be the
+#'     entity `@id`, if a `list` is given, this is assumed to be a complete
+#'     entity.
+#' @param ... Optional additional entity values/properties. Used when `x` is
+#'     NOT a `list`.
 #' @param ent_name String with the name of the entity.
 #' @param required Vector with list of keys required for the entity to be valid.
 #'     (default: `c("@id", "@type")`)
 #'
 #' @returns Boolean value to indicate if the given entity is valid.
 #' @keywords internal
-.validate_entity <- function(ent, ent_name = NULL, required = c("@id", "@type")) {
-  has_elements <- required %in% names(ent)
+.validate_entity <- function(x, ..., ent_name = NULL, required = c("@id", "@type")) {
+  UseMethod(".validate_entity", x)
+}
+
+#' @keywords internal
+.validate_entity.character <- function(x, ..., ent_name = NULL, required = "type") {
+  has_elements <- sapply(required, \(x) !is.null(getElement(list(...), x)))
+  has_elements |>
+    .validate_entity_overview(required, ent_name)
+}
+
+#' @keywords internal
+.validate_entity.list <- function(x, ..., ent_name = NULL, required = c("@id", "@type")) {
+  has_elements <- required %in% names(x)
+  has_elements |>
+    .validate_entity_overview(required, ent_name)
+}
+
+#' @keywords internal
+.validate_entity.numeric <- function(x, ..., ent_name = NULL, required = "type") {
+  has_elements <- sapply(required, \(x) !is.null(getElement(list(...), x)))
+  has_elements |>
+    .validate_entity_overview(required, ent_name)
+}
+
+#' Entity validation overview
+#'
+#' @inheritParams .validate_entity
+#'
+#' @returns Boolean flag with result of entity validation
+#' @keywords internal
+.validate_entity_overview <- function(has_elements, required, ent_name = NULL) {
   if (all(has_elements))
     return(TRUE)
 
   # In case there are missing elements from those `required`
+  msg <- ""
   if (!is.null(ent_name))
-    message("===== Checking: ", ent_name, " =====")
-  message("Missing: \n", paste0("- ", required[!has_elements], collapse = "\n"))
-  return(FALSE)
+    msg <- paste0("===== Checking: ", ent_name, " =====\n")
+  msg <- paste0(msg, "Missing: \n", paste0("- ", required[!has_elements], collapse = "\n"))
+  stop(msg)
 }
+
