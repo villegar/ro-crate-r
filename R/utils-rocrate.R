85256cdf96e5dc1b340ec15ba7310ff57dcc8eef
Roberto Villegas-Diaz <r.villegas-diaz@liverpool.ac.uk>
2025-08-12 13:25:34 +0100

    Update example for is_rocrate and update validation steps

--- R/utils-rocrate.R
+++ R/utils-rocrate.R
@@ -1,10 +1,16 @@
-#' Check if a given has the `rocrate` class
+#' Check if object is an RO-Crate
 #'
 #' @param rocrate RO-Crate object, see [rocrateR::rocrate].
 #'
-#' @returns Nothing, call for its side effect.
+#' @returns Returns invisibly the input RO-Crate object.
+#' @export
 #'
-#' @keywords internal
+#' @examples
+#' basic_crate <- rocrateR::rocrate()
+#'
+#' # check if the new crate is valid
+#' basic_crate |>
+#'   rocrateR::is_rocrate()
 is_rocrate <- function(rocrate) {
   # has the 'rocrate' class
   has_rocrate_class <- "rocrate" %in% class(rocrate)
@@ -12,22 +18,29 @@ is_rocrate <- function(rocrate) {
   # extract main value for @context
   ro_crate_context <- getElement(rocrate, "@context")
 
-  # has a valid @context -- TOO SLOW
-  # has_valid_context <- ro_crate_context |>
-  #   sapply(\(x) RCurl::url.exists(x)) |>
-  #   any()
-  has_valid_context <- TRUE
+  # check if @context is missing
+  missing_context <- is.null(ro_crate_context)
+
+  # has a valid @context
+  has_valid_context <- ro_crate_context |>
+    sapply(.is_valid_url, suffix = "/context") |>
+    any() # at least one entry of the RO-Crate context must be a valid URL
 
   # extract @graph
   ro_crate_graph <- getElement(rocrate, "@graph")
 
+  # check if @graph is missing
+  missing_graph <- is.null(ro_crate_graph)
+
   # extract @graph elements' @id
   graph_ids <- ro_crate_graph |>
-    sapply(`[[`, "@id")
+    sapply(`[[`, "@id") |>
+    unlist()
 
   # extract @graph elements' @type
   graph_types <- ro_crate_graph |>
-    sapply(`[[`, "@type")
+    sapply(`[[`, "@type") |>
+    unlist()
 
   # check lengths of @ids and @types, must be the same
   valid_length_graph <- length(graph_ids) == length(graph_types)
@@ -40,22 +53,27 @@ is_rocrate <- function(rocrate) {
 
   msg <- ""
   if (!has_rocrate_class)
-    msg <- "- Missing 'rocrate' class.\n"
-  if (!has_valid_context)
-    msg <- paste0(msg, "- Invalid value for @context: ", paste0(ro_crate_context, collapse = "; "), ".\n")
-  if (is.null(ro_crate_graph))
-    msg <- paste0(msg, "- The @graph section is missing.\n")
-  if (!valid_length_graph)
-    msg <- paste0(msg, "- The entities in @graph are NOT valid (e.g., missing @id and/or @type).\n")
-  if (!has_rocrate_meta)
-    msg <- paste0(msg, "- Missing the entity for the RO-Crate Metadata descriptor, ",
+    msg <- "    - Missing 'rocrate' class.\n"
+  if (missing_context)
+    msg <- paste0(msg, "    - Missing @context.\n")
+  if (!missing_context && !has_valid_context)
+    msg <- paste0(msg, "    - Invalid value for @context: ", paste0(ro_crate_context, collapse = "; "), ".\n")
+  if (missing_graph)
+    msg <- paste0(msg, "    - Missing @graph.\n")
+  if (!missing_graph && !valid_length_graph)
+    msg <- paste0(msg, "    - The entities in @graph are NOT valid (e.g., missing @id and/or @type).\n")
+  if (!missing_graph && !has_rocrate_meta)
+    msg <- paste0(msg, "    - Missing the entity for the RO-Crate Metadata descriptor, ",
                   "@id = 'ro-crate-metadata.json'.\n")
-  if (!has_root)
-    msg <- paste0(msg, "- Missing the root entity, @id = './'.\n")
+  if (!missing_graph && !has_root)
+    msg <- paste0(msg, "    - Missing the root entity, @id = './'.\n")
   if (nchar(msg) > 0) {
-    stop("Invalid RO-Crate object! Try running `rocrate()`, first.\n",
-         "Identified issue(s):\n",
+    stop("Invalid RO-Crate object! Try running `rocrateR::rocrate()`, first.\n",
+         "  Identified issue(s):\n",
          msg)
   }
+
+  # return (invisibly) the input RO-Crate
+  return(invisible(rocrate))
 }
 
